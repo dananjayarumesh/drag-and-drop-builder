2fb3aa1c72b07a8bb1ab1fd69a222583a950af68
dananjayarumesh <rumeshpriv@gmail.com>
2025-01-16 10:40:25 +0000

    project init, vue draggable added

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import draggable from 'vuedraggable'
+
+const list1 = ref([
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
+  { id: 4, name: "Item 4" },
+]);
+
+const list2 = ref([
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 66" },
+]);
+
+const onDragEnd = () => {
+  console.log("Drag ended. Final list order:", list2.value);
+};
+</script>
+
+<template>
+  <div class="grid grid-cols-[20%_80%]">
+    <div class="draggable-container">
+      <draggable v-model="list1" class="draggable-list" ghost-class="ghost" @end="onDragEnd">
+        <template #item="{ element }">
+          <div class="draggable-item">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
+    <div>
+      <draggable v-model="list2" class="draggable-list" ghost-class="ghost" @end="onDragEnd">
+        <template #item="{ element }">
+          <div class="draggable-item">
+            {{ element.name }}
+          </div>
+        </template>
+      </draggable>
+    </div>
+  </div>
+</template>
